f36a4f2aa00898bde754469bfd7ae2d53d4854f0
Onic <125625974+Onicoiho@users.noreply.github.com>
2023-03-15 14:28:28 +0800

    Add files via upload

---
+++ SendEmail.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="SendEmail.aspx.vb" Inherits="SendingMail.SendEmail" ValidateRequest="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="container">
+        <asp:TextBox ID="txtmsg" runat="server" Height="300px"  Width="500px" TextMode="MultiLine"></asp:TextBox><br /><br />
+
+        <asp:Button ID="btnSendMail" runat="server" Text="Send" /> <br /><br />
+
+        <asp:Label ID="lblStatus" runat="server" Text=""></asp:Label>
+
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ SendEmail.aspx.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Net
+Imports System.Net.Mail
+Imports System.Net.Configuration
+Public Class SendEmail
+    Inherits System.Web.UI.Page
+
+    Private Sub btnSendMail_Click(sender As Object, e As EventArgs) Handles btnSendMail.Click
+        SendEmail(txtmsg.Text)
+    End Sub
+
+    Protected Sub SendEmail(p As String)
+        Dim strTo = "coromanowebmaster@gmail.com"
+        Dim strSubject = "SUBJECT"
+        Dim strBody = p
+
+        Dim smtpSection As SmtpSection = CType(ConfigurationManager.GetSection("system.net/mailSettings/smtp"), SmtpSection)
+        Using mm As MailMessage = New MailMessage(smtpSection.From, strTo)
+            mm.Subject = strSubject
+            mm.Body = strBody
+            mm.IsBodyHtml = True
+            Dim smtp As SmtpClient = New SmtpClient
+            smtp.Host = smtpSection.Network.Host
+            smtp.EnableSsl = smtpSection.Network.EnableSsl
+            Dim networkCred As NetworkCredential = New NetworkCredential(smtpSection.Network.UserName, smtpSection.Network.Password)
+            smtp.UseDefaultCredentials = smtpSection.Network.DefaultCredentials
+            smtp.Credentials = networkCred
+            smtp.Port = smtpSection.Network.Port
+            smtp.Send(mm)
+            txtmsg.Text = ""
+            lblStatus.Text = "Your email has been sent sucessfully."
+        End Using
+    End Sub
+
+    Private Sub SendEmail(p1 As String, p2 As String)
+        Throw New NotImplementedException
+    End Sub
+End Class
